6a05006cbb98c5381dd8942c879af4fc580f1f52
Codeddy <buenosotoe@gmail.com>
2025-05-25 01:44:23 +0200

    Commit inicial. El proyecto está configurado para que cargue la página de Web en contrucción.

---
+++ resources/views/temporalView.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>JustTrack</title>
+        <link rel="stylesheet" href="style.css">
+        @vite('resources/css/temporalView.css')
+    </head>
+    <body>
+        <main>
+            <h1>Bienvenid@ a <span>JustTrack</span></h1>
+            <p>La web está en construcción. JustTrack es mi TFG (Trabajo de Fin de Grado). Se trata de un servicio de tracking de contenido multimedia online; podrás crearte una watchlist de películas y series que has visualizado y cuáles quieres visualizar de cara a futuro.</p>
+            <a href="https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/53890/article_full%403x.jpg">URL a la imagen de fondo</a>
+        </main>
+    </body>
+</html>
