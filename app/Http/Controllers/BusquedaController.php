8defa95262523404b957b64e5726badadef46c03
Codeddy <buenosotoe@gmail.com>
2025-06-12 02:54:02 +0200

    Ahora los botones de interacción de cada película son completamente funcionales también para la vista que muestra los resultados de las búsquedas.

--- app/Http/Controllers/BusquedaController.php
+++ app/Http/Controllers/BusquedaController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Listas;
 use App\Models\Reproducibles;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BusquedaController extends Controller
 {
@@ -21,6 +23,22 @@ class BusquedaController extends Controller
             $peliculas = Reproducibles::all();
         }
 
-        return view('busquedaView', compact('peliculas'));
+        // Obtenemos el usuario con el que la sesión está iniciada.
+        $usuario = Auth::user();
+
+        // Obtenemos IDs de las películas por cada lista del usuario
+        $listas = Listas::where('id_propietario', $usuario->id)
+            ->with('reproducibles:id') // Solo IDs
+            ->get()
+            ->keyBy('nombre'); // Para acceder por nombre: 'Visto', 'Me gusta', etc.
+
+        $idsPorLista = [
+            'Watchlist' => $listas['Watchlist']->reproducibles->pluck('id')->toArray(),
+            'Visto' => $listas['Visto']->reproducibles->pluck('id')->toArray(),
+            'Me gusta' => $listas['Me gusta']->reproducibles->pluck('id')->toArray(),
+            'No me gusta' => $listas['No me gusta']->reproducibles->pluck('id')->toArray(),
+        ];
+
+        return view('busquedaView', compact('peliculas', 'idsPorLista'));
     }
 }
--- resources/views/busquedaView.blade.php
+++ resources/views/busquedaView.blade.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>JustTrack - Búsqueda</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('storage/favicon/favicon.ico') }}">
-    @vite(['resources/css/header.css', 'resources/css/general.css', 'resources/css/busquedaView.css', 'resources/js/animacion_texto_ancho.js'])
+    @vite(['resources/css/header.css', 'resources/css/general.css', 'resources/css/busquedaView.css', 'resources/js/animacion_texto_ancho.js', 'resources/js/botones_listas.js'])
 </head>
 
 <body>
@@ -25,16 +26,24 @@
                 @if (auth()->check())
                 <div class="interacciones_peliculas">
                     <button title="Marcar como 'Lo quiero ver'" id="boton_watchlist_{{ $pelicula->id }}">
-                        <img src="{{ asset('storage/botones_interaccion/contenido_no_marcado_icon.svg') }}" alt="">
+                        <img src="{{ in_array($pelicula->id, $idsPorLista['Watchlist']) 
+                    ? asset('storage/botones_interaccion/contenido_marcado_icon.svg') 
+                    : asset('storage/botones_interaccion/contenido_no_marcado_icon.svg') }}" alt="">
                     </button>
                     <button title="Marcar como 'Visto'" id="boton_visto_{{ $pelicula->id }}">
-                        <img src="{{ asset('storage/botones_interaccion/contenido_no_visto_icon.svg') }}" alt="">
+                        <img src="{{ in_array($pelicula->id, $idsPorLista['Visto']) 
+                    ? asset('storage/botones_interaccion/contenido_visto_icon.svg') 
+                    : asset('storage/botones_interaccion/contenido_no_visto_icon.svg') }}" alt="">
                     </button>
                     <button title="Marcar como 'Me gusta'" id="boton_like_{{ $pelicula->id }}">
-                        <img src="{{ asset('storage/botones_interaccion/contenido_sin_like_icon.svg') }}" alt="">
+                        <img src="{{ in_array($pelicula->id, $idsPorLista['Me gusta']) 
+                    ? asset('storage/botones_interaccion/contenido_con_like_icon.svg') 
+                    : asset('storage/botones_interaccion/contenido_sin_like_icon.svg') }}" alt="">
                     </button>
                     <button title="Marcar como 'No me gusta'" id="boton_dislike_{{ $pelicula->id }}">
-                        <img src="{{ asset('storage/botones_interaccion/contenido_sin_dislike_icon.svg') }}" alt="">
+                        <img src="{{ in_array($pelicula->id, $idsPorLista['No me gusta']) 
+                    ? asset('storage/botones_interaccion/contenido_con_dislike_icon.svg') 
+                    : asset('storage/botones_interaccion/contenido_sin_dislike_icon.svg') }}" alt="">
                     </button>
                 </div>
                 @endif
