7763b8bd062379e6db7ca4f75e48f2dc65b1a159
Codeddy <buenosotoe@gmail.com>
2025-06-15 00:47:33 +0200

    Ahora la sección popular representa las películas en orden de calificación de mejor a peor, y luego de fecha más reciente a más antíguas.

--- app/Http/Controllers/PopularController.php
+++ app/Http/Controllers/PopularController.php
@@ -6,13 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Models\Listas;
 use App\Models\Reproducibles;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PopularController extends Controller
 {
     // Muestra la página "popular".
     public function showPopular() {
-        // Obtenemos todas las películas de la base de datos.
-        $peliculas = Reproducibles::all();
+        // Obtenemos todas las películas de la base de datos ordenadas por media de calificación y luego por fecha de lanzamiento.
+        $peliculas = Reproducibles::leftJoin('CALIFICACIONES_REPRODUCIBLES', 'REPRODUCIBLES.id', '=', 'CALIFICACIONES_REPRODUCIBLES.id_reproducible')
+            ->select('REPRODUCIBLES.id', 'REPRODUCIBLES.nombre', 'REPRODUCIBLES.fecha_lanzamiento', 'REPRODUCIBLES.url_caratula', DB::raw('AVG(CALIFICACIONES_REPRODUCIBLES.calificacion) as media_calificacion'))
+            ->groupBy('REPRODUCIBLES.id', 'REPRODUCIBLES.nombre', 'REPRODUCIBLES.fecha_lanzamiento', 'REPRODUCIBLES.url_caratula')
+            ->orderByDesc('media_calificacion')
+            ->orderByDesc('REPRODUCIBLES.fecha_lanzamiento')
+            ->get();
 
         if (Auth::check()) {
             // Obtenemos el usuario con el que la sesión está iniciada.
